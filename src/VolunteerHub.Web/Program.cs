2743a688097733a24e48d0f14a9ef44c1453795d
DungPham <phamtiendung2k5hc@gmail.com>
2026-04-06 17:06:55 +0700

    Initial commit

---
+++ src/VolunteerHub.Web/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using VolunteerHub.Infrastructure;
+using VolunteerHub.Infrastructure.Persistence;
+using VolunteerHub.Web.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+await VolunteerHub.Infrastructure.Seed.RoleSeeder.SeedAsync(app.Services);
+await VolunteerHub.Infrastructure.Persistence.Seeding.BadgeSeeder.SeedAsync(app.Services);
+app.UseExceptionHandler();
+if (!app.Environment.IsDevelopment()) app.UseHsts();
+app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
