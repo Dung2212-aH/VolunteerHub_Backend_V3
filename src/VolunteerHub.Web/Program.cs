4f80a9221ad01cff70b2cdbac256dd47313be612
DungPham <phamtiendung2k5hc@gmail.com>
2026-04-07 17:16:00 +0700

    end version

--- src/VolunteerHub.Web/Program.cs
+++ src/VolunteerHub.Web/Program.cs
@@ -4,6 +4,7 @@ using VolunteerHub.Infrastructure.Persistence;
 using VolunteerHub.Web.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -11,20 +12,43 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowFrontend", policy =>
+//     {
+//         policy.WithOrigins("http://localhost:5173")
+//               .AllowAnyHeader()
+//               .AllowAnyMethod()
+//               .AllowCredentials();
+//     });
+// });
+
 var app = builder.Build();
+
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     await dbContext.Database.MigrateAsync();
 }
+
 await VolunteerHub.Infrastructure.Seed.RoleSeeder.SeedAsync(app.Services);
 await VolunteerHub.Infrastructure.Persistence.Seeding.BadgeSeeder.SeedAsync(app.Services);
+
 app.UseExceptionHandler();
-if (!app.Environment.IsDevelopment()) app.UseHsts();
+
+if (!app.Environment.IsDevelopment())
+    app.UseHsts();
+
 app.UseHttpsRedirection();
+
+// app.UseCors("AllowFrontend");
+
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
